f51010201a9514087c3501c1ecaa941a0a03769d
Nived <67339427+Nived151@users.noreply.github.com>
2023-09-09 17:59:26 -0500

    struct correct

--- Project 1/fulladder_tb_four.v
+++ Project 1/fulladder_tb_four.v
@@ -11,7 +11,7 @@ module testbench_four;
     wire Cout;
     
     // Instantiate the 4-bit full adder
-    fulladder_behav_four dut (
+    fulladder_struct_four dut (
         .A(A),
         .B(B),
         .Cin(Cin),
@@ -26,18 +26,24 @@ module testbench_four;
     // Stimulus
     initial begin
         //$display("Time\tA\tB\tCin\tSum\tCout");
-        $dumpfile("testbench_four_behav.vcd");  
+        $dumpfile("testbench_four_struct.vcd");  
         $dumpvars(0, testbench_four);
         
         A = 4'b0000; B = 4'b0000; Cin = 0; // 0 0 0
         #10;
         
-        A = 4'b0101; B = 4'b0101; Cin = 1; // 0
+        A = 4'b0110; B = 4'b0110; Cin = 0; // 0
         #10;
         
-        A = 4'b0000; B = 4'b0001; Cin = 0;
+        A = 4'b0111; B = 4'b0111; Cin = 0;
         #10;
         
+        A = 4'b1011; B = 4'b1011; Cin = 0;
+        #10;
+
+        A = 4'b1100; B = 4'b1100; Cin = 0;
+        #10;
+
         A = 4'b1111; B = 4'b1111; Cin = 1;
         #10;
         
