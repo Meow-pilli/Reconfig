e8bcc7bf5f6217f86a66601acd926932eb10b53f
Chandanam <sxc210186@utdallas.edu>
2023-09-13 18:52:56 -0500

    generated jic file

--- Project 2.1/ledcounter1.v
+++ Project 2.1/ledcounter1.v
@@ -1,4 +1,4 @@
-module ledcounter (
+module ledcounter1 (
     input wire clk,
     input wire reset,
     input wire direction,
@@ -6,11 +6,18 @@ module ledcounter (
 );
     reg [31:0] pre_count;
 
-    always @(posedge clk or negedge reset)
-	 begin        if (!reset) begin            pre_count <= 32'b0;
-        end else begin            if (direction) begin                pre_count <= pre_count + 1;
-            end else begin                pre_count <= pre_count - 1;
-            end        end
+ 
+
+    always @(posedge clk or negedge reset) begin
+        if (!reset) begin
+            pre_count <= 32'b0;
+        end else begin
+            if (direction) begin
+                pre_count <= pre_count + 1;
+            end else begin
+                pre_count <= pre_count - 1;
+            end
+        end
         count <= pre_count[31:28]; // assign the 4 most significant bits to the output
     end
 endmodule
